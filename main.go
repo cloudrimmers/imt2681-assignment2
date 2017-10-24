dadac47884187be5bb4ba6adf25393e395344f62
Jonas J. Solsvik <jonas.solsvik@gmail.com>
2017-10-24 17:25:55 +0200

    Added dump/ and .eng

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	// Example: router.HandleFunc("/projectinfo/v1/github.com/{user}/{repo}", gitRepositoryHandler)
+
+	log.Println(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
