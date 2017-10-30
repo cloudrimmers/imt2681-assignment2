6e2204fd9411ca1cc8e2bd01b5b3867d5c9e70cc
Jonas J. Solsvik <jonas.solsvik@gmail.com>
2017-10-30 18:44:22 +0100

    changed log.Fatal -> log.Println

--- cmd/fixerworker/main.go
+++ cmd/fixerworker/main.go
@@ -22,7 +22,7 @@ func fixer2mongo(
 	// 1. Connect and request to fixer.io
 	resp, err := http.Get(fixerURI)
 	if err != nil {
-		log.Fatal("Wrong contact with: "+fixerURI+" ...", err.Error())
+		log.Println("Wrong contact with: "+fixerURI+" ...", err.Error())
 		return
 	}
 
@@ -30,14 +30,14 @@ func fixer2mongo(
 	payload := &(payload.FixerIn{})
 	err = json.NewDecoder(resp.Body).Decode(payload)
 	if err != nil {
-		log.Fatal("Could not decode resp.Body...", err.Error())
+		log.Println("Could not decode resp.Body...", err.Error())
 		return
 	}
 
 	// 3. Connect to DB
 	session, err := mgo.Dial(mongoURI)
 	if err != nil {
-		log.Fatal("No connection with mongodb @ ", mongoURI, err.Error())
+		log.Println("No connection with mongodb @ ", mongoURI, err.Error())
 		return
 	}
 	defer session.Close()
@@ -46,26 +46,10 @@ func fixer2mongo(
 	now := time.Now()
 	payload.Datestamp = fmt.Sprintf("%d-%02d-%02d", now.Year(), now.Month(), now.Day())
 
-	// 5. Check if already exist
-	collection := session.DB(mongoDB).C(mongoC)
-	query, _ := json.Marshal(struct {
-		Base      string
-		Datestamp string
-	}{
-		payload.Base,
-		payload.Datestamp,
-	})
-
-	log.Println(string(query))
-	if collection.Find(string(query)) == nil {
-		log.Fatal("Already exist...")
-		return
-	}
-
-	// 6. Dump payload to database
+	// 5. Dump payload to database
 	err = session.DB(mongoDB).C(mongoC).Insert(payload)
 	if err != nil {
-		log.Fatal("Error on db.Insert():\n", err.Error())
+		log.Println("Error on db.Insert():\n", err.Error())
 		return
 	}
 
