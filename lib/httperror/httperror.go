621b7a763e27dda3772344e24f9a1f3461e752b8
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-11-07 21:36:52 +0100

    factored http error codes into a separate module

--- lib/httperror/httperror.go
+++ lib/httperror/httperror.go
@@ -1,26 +1,30 @@
-package main
+package httperror
 
 import (
 	"log"
 	"net/http"
 )
 
-func serviceUnavailable(w http.ResponseWriter, err error) {
+// ServiceUnavailable ...
+func ServiceUnavailable(w http.ResponseWriter, err error) {
 	log.Println("No database connection: ", err.Error())
 	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
 }
 
-func internalServerError(w http.ResponseWriter, err error) {
+// InternalServer ...
+func InternalServer(w http.ResponseWriter, err error) {
 	log.Println("Collection.Insert() error", err.Error())
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
-func notFound(w http.ResponseWriter, err error) {
+// NotFound ...
+func NotFound(w http.ResponseWriter, err error) {
 	log.Println("Collection.Find() not found", err.Error())
 	http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 }
 
-func badRequest(w http.ResponseWriter, err error) {
+// BadRequest ...
+func BadRequest(w http.ResponseWriter, err error) {
 	log.Println("Http bad request", err.Error())
 	http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 }
