83286f44b45884990062a41c24b6868fc1dce8b5
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-11-13 21:19:26 +0100

    fixerworker 73%

--- cmd/fixerworker/main.go
+++ cmd/fixerworker/main.go
@@ -68,8 +68,15 @@ func main() {
 
 		if targetWait >= 0 {
 			targetWait = -(timetool.UntilTomorrow())
+			response, err := APP.FixerResponse(APP.FixerioURI)
 
-			APP.Fixer2Mongo()
+			if err != nil {
+				log.Println("ERROR FixerResponse()")
+			} else if err = APP.Fixer2Mongo(response); err != nil {
+				log.Println("ERROR Fixer2Mongo()")
+			} else {
+				log.Println("SUCCESS pulling fixer.io: ", response)
+			}
 		}
 	}
 }
