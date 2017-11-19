d350e355a0b365579c00c1f202c6cb2d7aae743f
Halvor Bakken Smedås <stektpotet@gmail.com>
2017-11-19 21:53:53 +0100

    reworked DialogFlow 'API' to be independent, as libraries should be.

--- lib/dialogFlow/dialogFlow_test.go
+++ lib/dialogFlow/dialogFlow_test.go
@@ -2,30 +2,63 @@ package dialogFlow
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
 	"testing"
-	"time"
 
-	"github.com/cloudrimmers/imt2681-assignment3/lib/benchmark"
-	"github.com/cloudrimmers/imt2681-assignment3/lib/validate"
+	"github.com/cloudrimmers/imt2681-assignment3/lib/reflectUtil"
+	"github.com/cloudrimmers/imt2681-assignment3/lib/types"
+	"github.com/subosito/gotenv"
 )
 
-const dialogFlowSampleFile = "./testData/dialogFlowSample.json"
+const dialogFlowTestSamples = "./testData/"
 
 func DoRequestHere(req *http.Request) (resp *http.Response, err error) {
 	resp = &http.Response{}
 	resp.StatusCode = http.StatusOK
 	resp.Header = http.Header{}
 	resp.Header.Add("Content-Type", "application/json")
-	data, err := ioutil.ReadFile(dialogFlowSampleFile)
+	data, err := ioutil.ReadFile(dialogFlowTestSamples + "OK.json")
 	if err != nil {
 		panic(err)
 	}
 	resp.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+	return
+}
+
+func DoUnauthorizedRequestHere(req *http.Request) (resp *http.Response, err error) {
+	resp = &http.Response{}
+	resp.StatusCode = http.StatusUnauthorized
+	resp.Header = http.Header{}
+	resp.Header.Add("Content-Type", "application/json")
+	data, err := ioutil.ReadFile(dialogFlowTestSamples + "Unauthorized.json")
+	if err != nil {
+		panic(err)
+	}
+	resp.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+	return
+}
+
+func DoBadRequestHere(req *http.Request) (resp *http.Response, err error) {
+	resp = &http.Response{}
+	resp.StatusCode = http.StatusBadRequest
+	resp.Header = http.Header{}
+	resp.Header.Add("Content-Type", "application/json")
+	data, err := ioutil.ReadFile(dialogFlowTestSamples + "BadRequest.json")
+	if err != nil {
+		panic(err)
+	}
+	resp.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+
+	return
+}
+
+func DoRequestError(req *http.Request) (resp *http.Response, err error) {
+	resp = &http.Response{}
+	err = fmt.Errorf("Something went wrong u big dumdum")
 	return
 }
 
@@ -67,53 +100,94 @@ type queryRequesterGen struct {
 	rq  requester
 }
 
-func TestQuery(t *testing.T) {
+func TestDialogFlowDependencyFail(t *testing.T) {
 
-	defer benchmark.Benchmark(time.Now())
-	parser := func(in, out string, amount float64) (gen queryRequesterGen) {
-		gen.qry = fmt.Sprintf("%v %v to %v", amount, in, out)
-		gen.rq = func(req *http.Request) (resp *http.Response, err error) {
-			resp = &http.Response{}
-			resp.StatusCode = http.StatusOK
-			resp.Header = http.Header{}
-			resp.Header.Add("Content-Type", "application/json")
-			data, err := ioutil.ReadFile(dialogFlowSampleFile)
-			if err != nil {
-				panic(err)
-			}
-			responseData := Response{}
-			json.Unmarshal(data, &responseData)
-			responseData.Query = fmt.Sprintf("%v %v to %v", amount, in, out)
-			err = validate.Currency(in)
-			if err == nil {
-				responseData.Result.Parameters.CurrencyIn.CurrencyName = in
-			}
-			err = validate.Currency(out)
-			if err == nil {
-				responseData.Result.Parameters.CurrencyIn.CurrencyName = out
-			}
-			responseData.Result.Parameters.Amount = amount
-			raw, err := json.Marshal(responseData)
-			if err != nil {
-				panic(err)
-			}
-			resp.Body = ioutil.NopCloser(bytes.NewBuffer(raw))
-			return
-		}
-		return
-	}
-	tests := []struct {
-		Name      string
-		Generator queryRequesterGen
-	}{
-		{"Test 1", parser("Nok", "GBP", 23.56)},
-		{"Test 2", parser("GBHK", "GBP", 23.56)},
-		{"Test 3", parser("Nok", "GBP", 23.56)},
+	testOut := types.Response{}
+	status := doQuery(nil, DoRequestError, &testOut, "")
+
+	if status != http.StatusFailedDependency {
+		t.Errorf("%s = %v, want %v", reflectUtil.GetCallerNameInTest(), status, http.StatusFailedDependency)
 	}
 
-	for _, test := range tests {
-		t.Run(test.Name, func(t *testing.T) {
-			doQuery(test.Generator.qry, test.Generator.rq)
-		})
+	t.Logf("%+v", testOut)
+}
+
+func TestBadRequest(t *testing.T) {
+
+	testOut := types.Response{}
+	status := doQuery(nil, DoBadRequestHere, &testOut, "")
+
+	if status != http.StatusBadRequest {
+		t.Errorf("%s = %v, want %v", reflectUtil.GetCallerNameInTest(), status, http.StatusBadRequest)
 	}
+
+	t.Logf("%+v", testOut)
+}
+
+func TestUnauthorizedRequest(t *testing.T) {
+	testOut := types.Response{}
+	status := doQuery(nil, DoUnauthorizedRequestHere, &testOut, "")
+	if status != http.StatusUnauthorized {
+		t.Errorf("%s = %v, want %v", reflectUtil.GetCallerNameInTest(), status, http.StatusUnauthorized)
+	}
+	t.Logf("%+v", testOut)
+}
+
+func TestValidQuery(t *testing.T) {
+	testOut := types.Response{}
+	qry := &query{
+		Language:  "en",
+		Query:     "Convert 200 nok to USD",
+		SessionID: "1",
+		Contexts:  nil,
+	}
+	status := doQuery(qry, DoRequestHere, &testOut, "")
+
+	if status != http.StatusOK {
+		t.Errorf("%s = %v, want %v", reflectUtil.GetCallerName(), status, http.StatusOK)
+	}
+	t.Logf("%+v", testOut)
+}
+
+func TestWrongSessionID(t *testing.T) {
+	testOut := types.Response{}
+	qry := &query{
+		Language:  "en",
+		Query:     "Convert 200 nok to USD",
+		SessionID: "NOT THE SESSION ID YOU'RE LOOKING FOR",
+		Contexts:  nil,
+	}
+	status := doQuery(qry, DoRequestHere, &testOut, "")
+
+	if status != http.StatusUnauthorized {
+		t.Errorf("%s = %v, want %v", reflectUtil.GetCallerName(), status, http.StatusUnauthorized)
+	}
+
+	t.Logf("%+v", testOut)
+}
+
+//
+// func TestPartialContent(t *testing.T) {
+// 	testOut := struct{}{}
+//
+//
+// 	status := doQuery(nil, DoRequestHere, &testOut, "")
+//
+// 	if status != http.StatusPartialContent {
+// 		t.Errorf("%s = %v, want %v", reflectUtil.GetCallerName(), status, http.StatusPartialContent)
+// 	}
+//
+// 	t.Logf("%+v", testOut)
+// }
+
+//NOTE: Not really neccessary, but just to confirm nothing is wrong with our mock of dialogflow
+func TestReal(t *testing.T) {
+	gotenv.MustLoad("../../cmd/rimbot/.env")
+	testOut := types.Response{}
+	status := Query("Nok to Eur", &testOut, os.Getenv("ACCESS_TOKEN"))
+	if status != http.StatusOK {
+		t.Errorf("Actually connecting to dialogflow fails, Check if API is changed")
+		t.Errorf("%+v", testOut)
+	}
+	t.Logf("%+v", testOut)
 }
