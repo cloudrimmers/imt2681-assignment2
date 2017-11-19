52984c5950d6d386b0a89bc20ae886567028dd5c
JonShard <jonems@stud.ntnu.no>
2017-11-19 22:42:02 +0100

    Refactored the botnameGenerator package to use a better data structure.

--- lib/botNameGenerator/nameGen.go
+++ lib/botNameGenerator/nameGen.go
@@ -5,9 +5,11 @@ import (
 	"time"
 )
 
-var firstNames = map[int]string{0: "Sarah", 1: "Dave", 2: "Joe", 3: "Zoe", 4: "Peter", 5: "Liam", 6: "Fred", 7: "Lisa"}
-var adjectives = map[int]string{0: "happy", 1: "tall", 2: "sassy", 3: "giggely", 4: "jolly", 5: "cheerful", 6: "relaxing", 7: "wishful", 8: "festive"}
-var animalNames = map[int]string{0: "lion", 1: "zebra", 2: "lizzard", 3: "hamster", 4: "parrot", 5: "bear", 6: "penguin"}
+var firstNames = [...]string{"Sarah", "Dave", "Joe", "Zoe", "Peter", "Liam", "Fred", "Lisa", "martha", "Eric", "Christopher",
+	"Mariuz", "Simon", "Hans", "John", "Linda"}
+
+var adjectives = [...]string{"happy", "tall", "sassy", "giggely", "jolly", "cheerful", "relaxed", "wishful", "festive", "practical"}
+var animalNames = [...]string{"lion", "zebra", "lizzard", "hamster", "parrot", "bear", "penguin"}
 
 func Generate() (name string) {
 	rand.Seed(int64(time.Now().Nanosecond()))
