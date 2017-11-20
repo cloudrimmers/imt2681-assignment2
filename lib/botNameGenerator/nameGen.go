c512dc45c555706fd899eea1fc6a23866752f124
JonShard <jonems@stud.ntnu.no>
2017-11-20 15:35:54 +0100

    Added a bunch of names to the bot name genrator.

--- lib/botNameGenerator/nameGen.go
+++ lib/botNameGenerator/nameGen.go
@@ -8,16 +8,28 @@ import (
 var firstNames = [...]string{
 	"Sarah", "Dave", "Joe", "Zoe", "Peter", "Liam", "Fred", "Lisa", "Martha",
 	"Eric", "Christopher", "Mariuz", "Simon", "Hans", "John", "Linda", "Bob",
-	"Larry", "Cory", "Sam", "Lola", "Mary", "Dory", "Siri", "Karen", "Emiliy"}
+	"Larry", "Cory", "Sam", "Lola", "Mary", "Dory", "Siri", "Karen", "Emiliy",
+	"Chuck", "Tyrone", "Jon", "Womble", "Sasafras", "David", "Finn", "Jackson",
+	"Conan"}
 
 var adjectives = [...]string{
 	"happy", "tall", "sassy", "giggely", "jolly", "cheerful", "relaxed",
 	"wishful", "festive", "practical", "goofy", "lucky", "daft", "pretty",
-	"punky", "proper", "cheesy"}
+	"punky", "proper", "cheesy", "educated", "marvelous", "friendly", "genius",
+	"shifty-looking", "magical", "handy", "adorable", "cool", "heartwarming",
+	"weird", "bizzare", "memetastic", "uncomfortable", "savage", "bloody",
+	"slimy", "spooktastic", "triggered", "gender-fluid", "fully-armed", "cyborg",
+	"one-eyed", "three-eyed", "eagle-eyed", "cloudy", "air-headed", "enthusiastic",
+	"full-bearded", "untrustworthy", "spacy", "shady", "lazy-eyed", "greedy",
+	"confused", "international", "wise", "fishy", "speedy", "smelly", "exhausted"}
+
 var animalNames = [...]string{
 	"lion", "zebra", "lizzard", "hamster", "parrot", "bear", "penguin",
 	"horse", "monkey", "donkey", "owl", "dragon", "shark", "musox", "sheep",
-	"crocodile", "giraffe"}
+	"crocodile", "giraffe", "barbarian", "octompus", "raven", "eagle", "dwarf",
+	"fox", "wolf", "cowboy", "unicorn", "dragon", "snake", "zombie", "troll",
+	"starfish", "jellyfish", "eel", "bat", "gorilla", "deer", "seal", "squirel",
+	"fish", "pirate", "sloth", "otter", "hedgehog", "elephant", "skunk", "rabbit"}
 
 func Generate() (name string) {
 	rand.Seed(int64(time.Now().Nanosecond()))
